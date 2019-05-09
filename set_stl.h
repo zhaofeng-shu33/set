bc3edc6d211f1b21d2ded8c1d195d95eb3e2e98c
zhaofeng-shu33 <616545598@qq.com>
2019-05-09 11:32:48 +0800

    add partition class

--- set_stl.h
+++ set_stl.h
@@ -5,82 +5,82 @@
 #include <iterator>
 #include <iostream>
 namespace stl{
-    template <class T>
-    class Set {
-    public:
-        using element_type = T;
-        using value_type = element_type;
-        typedef typename std::set<element_type>::iterator iterator;
-        typedef typename std::set<element_type>::const_iterator const_iterator;
-        //! empty set
-        Set() {}
+template <class T>
+class Set {
+public:
+    using element_type = T;
+    using value_type = element_type;
+    typedef typename std::set<element_type>::iterator iterator;
+    typedef typename std::set<element_type>::const_iterator const_iterator;
+    //! empty set
+    Set() {}
 
-        std::size_t Cardinality() const {
-            return s.size();
+    std::size_t Cardinality() const {
+        return s.size();
+    }
+    bool HasElement(element_type i) const {
+        return static_cast<bool>(s.count(i));
+    }
+    bool IsSubSet(const Set& X) const {
+        for (element_type i : s) {
+            if (!X.HasElement(i))
+                return false;
         }
-        bool HasElement(element_type i) const {
-            return static_cast<bool>(s.count(i));
-        }
-        bool IsSubSet(const Set& X) const {
-            for (element_type i : s) {
-                if (!X.HasElement(i))
-                    return false;
+        return true;
+    }
+    void AddElement(element_type i) {
+        s.insert(i);
+    }
+    iterator insert(iterator position, const element_type& val) {
+        return s.insert(position, val);
+    }
+    iterator begin() {
+        return s.begin();
+    }
+    iterator end() {
+        return s.end();
+    }
+    const_iterator begin() const {
+        return s.begin();
+    }
+    const_iterator end() const {
+        return s.end();
+    }
+    bool IsEmpty() { return s.size() == 0; }
+    friend std::ostream& operator << (std::ostream& stream, const Set& X) {
+        stream << "{";
+        std::size_t cnt = 0, limiter = X.Cardinality();
+        for (Set::iterator i = X.begin(); i != X.end(); i++) {
+            stream << *i;
+            cnt++;
+            if (cnt != limiter) {
+                stream << ", ";
             }
-            return true;
         }
-        void AddElement(element_type i) {
-            s.insert(i);
-        }
-        iterator insert(iterator position, const element_type& val) {
-            return s.insert(position, val);
-        }
-        iterator begin() {
-            return s.begin();
-        }
-        iterator end() {
-            return s.end();
-        }
-        const_iterator begin() const {
-            return s.begin();
-        }
-        const_iterator end() const {
-            return s.end();
-        }
-        bool IsEmpty() { return s.size() == 0; }
-        friend std::ostream& operator << (std::ostream& stream, const Set& X) {
-            stream << "{";
-            std::size_t cnt = 0, limiter = X.Cardinality();
-            for (Set::iterator i = X.begin(); i != X.end(); i++) {
-                stream << *i;
-                cnt++;
-                if (cnt != limiter) {
-                    stream << ", ";
-                }
-            }
-            stream << "}";
-            return stream;
-        }
-        friend bool operator==(const Set& a, const Set& b) {
-            return a.s == b.s;
-        }
-        friend bool operator!=(const Set& a, const Set& b) {
-            return a.s != b.s;
-        }
+        stream << "}";
+        return stream;
+    }
+    friend bool operator==(const Set& a, const Set& b) {
+        return a.s == b.s;
+    }
+    friend bool operator!=(const Set& a, const Set& b) {
+        return a.s != b.s;
+    }
 
-        Set Union(const Set& X) const {
-            Set a;
-            std::set_union(X.begin(), X.end(), begin(), end(), std::inserter(a, a.begin()));
-            return a;
-        }
-        Set Intersection(const Set& X) const {
-            Set a;
-            std::set_intersection(X.begin(), X.end(), begin(), end(), std::inserter(a, a.begin()));
-            return a;
-        }
+    Set Union(const Set& X) const {
+        Set a;
+        std::set_union(X.begin(), X.end(), begin(), end(), std::inserter(a, a.begin()));
+        return a;
+    }
+    Set Intersection(const Set& X) const {
+        Set a;
+        std::set_intersection(X.begin(), X.end(), begin(), end(), std::inserter(a, a.begin()));
+        return a;
+    }
 
-    private:
-        std::set<element_type> s;
-    };
+private:
+    std::set<element_type> s;
+};
 //! a thin wrapper around std::set
 class CSet : public Set<std::size_t> {
 public:
@@ -117,5 +117,11 @@ public:
         return false;
     }
 };
-
+class Partition : public stl::Set<stl::CSet> {
+public:
+	static Partition makeFine(int size);
+	static Partition makeDense(int size);
+	Partition() {}
+	Partition expand(const stl::CSet& A);
+};
 }
